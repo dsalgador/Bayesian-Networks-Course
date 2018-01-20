683af1d382ef496a630c1e852bb7e189fb77723c
Daniel Salgado Rojo <daniel.salgado.rojo@gmail.com>
2018-01-20 10:23:10 +0100

    Add learning structure approach anc omparison between the only learning parameters approach

--- Delivery4/Delivery4_code_DaniSalgado.R
+++ Delivery4/Delivery4_code_DaniSalgado.R
@@ -22,7 +22,8 @@ par(mfrow = c(1,1))
                       dimnames=list(nodes(net), nodes(net)))
   #print(adjMatrix)
   amat(net) = adjMatrix
-
+  graphviz.plot(net)
+  
 #Load the data
   #getwd()
     load("datos_generados_ejercicio_7.rdata")  
@@ -42,8 +43,76 @@ par(mfrow = c(1,1))
 #Marginal of Y  
   print("Marginal of Y");print(coef$Y)
 # P(Z| Y = 0,X )
-  print("CPT of Y = 0");print(coef$Z[1,,])
+  print("CPT of Y = 0");print(coef$Z[,,1])
 # P(Z| Y = 1,X )
-  print("CPT of Y = 1");print(coef$Z[2,,]) 
+  print("CPT of Y = 1");print(coef$Z[,,2]) 
+
+#Estimations of the parameters:
+  theta_11 = coef$X[2]
+  theta_21 = coef$Y[2]
+  
+  theta_31 = coef$Z[2,2,2]    #P(Z = 1 / X = 1, Y = 1)
+  theta_32 = coef$Z[2,1,2]   #P(Z = 1 / X = 0, Y = 1)
+  theta_33 = coef$Z[2,2,1]    #P(Z = 1 / X = 1, Y = 0)
+  theta_34 = coef$Z[2,1,1]   #P(Z = 1 / X = 0, Y = 0)
+  
+ 
+###################################################################
+###################################################################
+
+#########################################
+# LEARNING the BN structure (DAG)        #
+######################################### 
+
+  net.pred <- hc(data, score = "bic")
+  graphviz.plot(net.pred)
+  #the arrows are inverted with respect the original structure
+  class(net.pred)
+  net.pred.estimated = bn.fit(net.pred, data, method = "mle")
+  #print(net.estimated)
+  
+  #Save coefficients
+  coef.pred <- coefficients(net.pred.estimated)
+  #dim(coef)
+  
+  print(net.pred.estimated)
+  
+################################
+# Example to compare the two cases
+#################################
+  library(gRain)  
+  #Assume we know that X = 1 (instantiate X=1),
+  #which is the probability of Z = 1 ?
+  
+  predictors <-c("X")
+  values <- c(1)
+  eevvii <- matrix(values, nrow = 1, byrow = T)
+  colnames(eevvii) <- predictors
+  evi <- as.data.frame(eevvii)
+  responses <- c("Z")
+  
+  #(A) net.estimated, when we imposed the structure
+  
+  net.estimated.gRain <- as.grain(net.estimated)
+  pp.estimated <- predict(net.estimated.gRain,
+          response = responses, 
+          newdata = evi,
+          predictor = predictors,
+          type = "class")
   
   
+  
+  #(B) net.pred.estimated, when we predicted the structure
+  net.pred.estimated.gRain <- as.grain(net.pred.estimated)
+  pp.pred.estimated <- predict(net.pred.estimated.gRain,
+                          response = responses, 
+                          newdata = evi,
+                          predictor = predictors,
+                          type = "class")
+
+#Comparison between the two approaches
+  
+print("How do differ both bayesian networks?")    
+print(all.equal(net.estimated, net.pred.estimated))
+print("Are both networks predicting the same and with the same CL?")
+print(all.equal(pp.estimated, pp.pred.estimated))
